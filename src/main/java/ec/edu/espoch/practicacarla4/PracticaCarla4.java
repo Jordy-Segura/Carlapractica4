0d2a2c21dfb0634eaa312e83e63dc2565755901c
jordy <marcelo2005jmsp@gmail.com>
2024-11-05 10:15:25 -0500

    Practica iniciada

---
+++ src/main/java/ec/edu/espoch/practicacarla4/PracticaCarla4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practicacarla4;
+
+/**
+ *
+ * @author jordy
+ */
+public class PracticaCarla4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
